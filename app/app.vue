aec5eee1d2e33f9b0398dd1a9fc14e3b5cfb9889
ChaDonSom <chadonsom@gmail.com>
2025-10-16 18:06:35 -0400

    8.5/11 card paper

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,5 @@
 <template>
-  <div class="h-screen w-screen bg-gray-200 overflow-hidden">No scrolling please</div>
+  <div class="h-screen w-screen bg-gray-200 overflow-hidden">
+    <Card> a card </Card>
+  </div>
 </template>
---
+++ app/components/Card.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="bg-white rounded-lg shadow p-4 aspect-[8.5/11] flex items-center justify-center">
+    <slot />
+  </div>
+</template>
