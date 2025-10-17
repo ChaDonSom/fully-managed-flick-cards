22fb6e28f2ae14aeef6a17e24a96a0310635d016
ChaDonSom <chadonsom@gmail.com>
2025-10-16 21:35:45 -0400

    touch hook

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,9 @@
+<script setup lang="ts">
+useTouchIngestion()
+</script>
+
 <template>
   <div class="h-screen w-screen bg-gray-200 overflow-hidden">
-    <Card v-for="i in 100" :key="i"> card {{ i }} </Card>
+    <Card v-for="i in 100" :key="i" :class="{ 'text-red-400': touching }"> card {{ i }} </Card>
   </div>
 </template>
---
+++ app/composables/use-touch-ingestion.ts
@@ -0,0 +1,39 @@
+declare global {
+  interface Window {
+    __touchIngestionRegistered?: boolean
+  }
+}
+
+export const touching = ref(false)
+/**
+ * This function registers touch event listeners to handle touch ingestion. It's the starting point to our touch
+ * processing pipeline. From its starting point, it will call everything else registered in the pipeline, in
+ * order, and allows each step to modify the event (or cancel it like preventDefault) as needed.
+ */
+export function useTouchIngestion() {
+  if (import.meta.server) return // No need to run on server
+
+  // No need to reregister if already registered
+  if (window.__touchIngestionRegistered) return
+
+  // Register touchstart listener
+  window.addEventListener(
+    "touchstart",
+    (event) => {
+      // Here you can add your touch processing logic
+      touching.value = true
+      console.log("Touch started", event)
+      // For example, you might want to call other functions in your touch processing pipeline
+    },
+    { passive: false } // Set to false if you plan to call preventDefault()
+  )
+  window.addEventListener("touchend", (event) => {
+    touching.value = false
+  })
+  window.addEventListener("touchmove", (event) => {})
+  window.addEventListener("touchcancel", (event) => {
+    touching.value = false
+  })
+
+  window.__touchIngestionRegistered = true
+}
